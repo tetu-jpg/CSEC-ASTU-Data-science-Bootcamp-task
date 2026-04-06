4089baff31bed71f180f4e166964b65ab96bb7f7
Tesnim Abdella <tesnimabdella52@gmail.com>
2026-04-06 13:09:21 +0300

    Add files via upload
    This code works to check whether given number is prime or not.

---
+++ prime.py
@@ -0,0 +1,14 @@
+n=int(input("enter a number:"))
+m=1
+count=0
+for i in range(n):
+    m+=1
+    if n%m==0:
+        count +=1
+if n==1:
+    print("neither composite nor prime")
+elif count <=2:
+    print('it is prime')
+else:
+    print("it is not prime")
+
